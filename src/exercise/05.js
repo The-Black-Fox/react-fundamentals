91578ee3b72392faa2cc2696b57e1837c8087abf
Josh Goldfarb <36429498+The-Black-Fox@users.noreply.github.com>
2022-12-29 17:00:14 -0400

    Finished Module 05

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -14,9 +14,64 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+const boxStyle = {
+  border: '1px solid #333',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  textAlign: 'center',
+}
+
+const Box = props => {
+  return (
+    <>
+      <div
+        className={`box ${props.className}`}
+        style={{...props.style, ...sizeofBox(props.size), fontStyle: 'italic'}}
+      >
+        {props.children}
+      </div>
+    </>
+  )
+}
+
+const sizeofBox = size => {
+  if (size === 'small') {
+    return {
+      width: '90px',
+      height: '90px',
+    }
+  } else if (size === 'medium') {
+    return {
+      width: '180px',
+      height: '180px',
+    }
+  } else if (size === 'large') {
+    return {
+      width: '270px',
+      height: '270px',
+    }
+  }
+  console.log(new Error('this is an invalid size type.'))
+
+  return
+}
+
+const smallBox = (
+  <Box size="small" style={{...boxStyle, backgroundColor: 'lightblue'}}>
+    small lightblue box
+  </Box>
+)
+const mediumBox = (
+  <Box size="medium" style={{...boxStyle, backgroundColor: 'pink'}}>
+    medium pink box
+  </Box>
+)
+const largeBox = (
+  <Box size="large" style={{...boxStyle, backgroundColor: 'orange'}}>
+    large orange box
+  </Box>
+)
 
 function App() {
   return (
